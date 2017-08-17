4cb93fb7d550e41a2d098b3f972f023ed0e22337
Vukadin <beogor@gmail.com>
2017-08-17 07:34:04 +0200

    progress

--- app.js
+++ app.js
@@ -4,51 +4,92 @@ new Vue({
 		fightLog: [],
 		players: [
 			{
-				name: 'Vukadin',
+				name: 'Priest',
 				health: 100,
 				baseDmg: 7,
 				criticalChance: 15.4,
-				critDmgMultiply: 3,
+				critDmg: 3,
 				dodgeChance: 5,
 				energy: 0,
-				healHealth: 10,
+				maxEnergy: 150,
+				healHealth: 20,
 				healCriticalChance: 2,
-				specialBase: "energy",
+				healCost: 5,
 				specialDmg: 3,
 				specialName: 'Holy Smite',
 				specialCost: 75
 			},
 			{
-				name: 'Fear',
+				name: 'Warlock',
 				health: 100,
 				baseDmg: 10,
 				criticalChance: 18,
 				critDmg: 3,
-				dodgeChance: 0,
+				dodgeChance: 10,
 				energy: 0,
-				healHealth: 0,
+				maxEnergy: 170,
+				healHealth: 13,
 				healCriticalChance: 0,
-				specialBase: 'Reversed opposite specialBase',
+				healCost: 5,
 				specialDmg: 3.2,
-				specialName: 'Spook',
+				specialName: 'Fear',
 				specialCost: 75
 			}
 		],
 		userPlayer: false,
 		robotPlayer: false,
-		choosed: false
+		choosed: false,
+		fightEnds: false,
+		winner: null
 	},
 	methods: {
-		attack: function(attacker, target) {
-			var dodged = chance(target.dodgeChance);
-			var dmg = attacker.baseDmg + chance(attacker.criticalChance) * attacker.critDmgMultiply;
+		attack: function(attacker, target, special = false) {
+			var dodged = this.chance(target.dodgeChance);
+			var dmg = attacker.baseDmg + this.chance(attacker.criticalChance) * attacker.critDmg;
 			var hitResult = dodged ? 0 : dmg;
-			var logResult = !hitResult ? target.name + ' doddged!' : hitResult + ' to ' + taget.name + '!';
+			var logResult = hitResult ? `${attacker.name} deals ${hitResult} damage to ${target.name}!` :  `${target.name} doddged!`;
+			if (special) {
+				attacker.energy -= attacker.specialCost;
+				dodge = false;
+				dmg = attacker.specialDmg * attacker.baseDmg;
+				hitResult = dmg;
+				logResult = `${attacker.name} casted ${attacker.specialName} at ${target.name} dealing ${hitResult} damage !`;
+			} else {
+				attacker.energy += 10;
+			}
 			target.health = target.health - hitResult;
 			this.fightLog.push(logResult);
+			this.fightComplete(attacker, target);
+			var vm = this;
+			setTimeout(function(){
+				vm.scrollLog();
+			}, 50);
+		},
+		heal: function(healer, healTarget = healer) {
+			healTarget.health += healer.healHealth;
+			var targetCheck = healer == healTarget;
+			var target = targetCheck ? 'himself' : healTarget.name;
+			healer.energy -= healer.healCost;
+			if(healTarget.health > healTarget.healthInit) {
+				healTarget.health = healTarget.healthInit;
+			}
+			this.fightLog.push(`${healer.name} healed ${target} by ${healer.healHealth} health!`);
 		},
 		chance: function(chance) {
-			return (Math.random() < (chance/100) ? 0 : 1);
+			var random = Math.random();
+			return (random < (chance/100) ? 1 : 0);
+		},
+		scrollLog: function() {
+			var log = this.$el.querySelector('.log');
+			log.scrollTop = log.scrollHeight;
+		},
+		fightComplete: function(attacker, target) {
+			if(target.health <= 0) {
+				this.fightEnds = true;
+				this.winner = attacker;
+				target.health = 0;
+				this.fightLog.push(`${attacker.name} kills ${target.name}!`);
+			}
 		},
 		choosePlayer: function(player){
 			if(this.userPlayer) {
@@ -65,6 +106,8 @@ new Vue({
 		for(var i = 0; i <= this.players.length - 1; i++) {
 			this.$set(this.players[i], 'showInfo', false);
 			this.$set(this.players[i], 'choosed', false);
+			this.$set(this.players[i], 'healthInit', this.players[i].health);
+			this.$set(this.players[i], 'healthPercent', this.players[i].health/this.players[i].healthInit *100);
 		}
 	},
 	created: function (player) {
